d2e7b523081bf90756689fa6e713343c1c7c184f
Arnaudgouel <gouel.arnaud@gmail.com>
2022-05-06 14:41:53 +0200

    api create and update for company address

--- src/Controller/CompanyAddressController.php
+++ src/Controller/CompanyAddressController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Entity\UserAddress;
+use App\Entity\Company;
+use App\Entity\CompanyAddress;
 use App\Service\ApiResponse;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UserAddressController extends AbstractController
+class CompanyAddressController extends AbstractController
 {
     private $em;
 
@@ -21,11 +21,11 @@ class UserAddressController extends AbstractController
         $this->em = $em;
     }
 
-    #[Route('/users/addresses', methods: "GET", name: 'app_user_address')]
+    #[Route('/companies/addresses', methods: "GET")]
     public function getAdresses(Request $request, ApiResponse $apiResponse): Response
     {
         $params = [
-            "user_id" => [
+            "company_id" => [
                 "type" => "integer"
             ],
         ];
@@ -34,21 +34,20 @@ class UserAddressController extends AbstractController
         if ($apiResponse->hasError) {
             return $this->json($response, 400, ['Content-Type' => 'application/json']);
         }
-        $userId = $response["user_id"];
-        $userAddress = $this->em->getRepository(UserAddress::class)->findAllByUser($userId);
-        return $this->json($userAddress);
+        $companyId = $response["company_id"];
+        $companyAddress = $this->em->getRepository(CompanyAddress::class)->findBy(["company_id" => $companyId]);
+        return $this->json($companyAddress);
     }
 
-    #[Route('/users/addresses', methods: "POST")]
+    #[Route('/companies/addresses', methods: "POST")]
     public function addAdresses(Request $request, ApiResponse $apiResponse): Response
     {
         $params = [
-            "user_id" => [
+            "company_id" => [
                 "type" => "integer",
             ],
             "name" => [
                 "type" => "string",
-                "required" => false
             ],
             "address_line1" => [
                 "type" => "string",
@@ -66,51 +65,51 @@ class UserAddressController extends AbstractController
             "country" => [
                 "type" => "string",
             ],
-            "phone_number" => [
-                "type" => "string",
-            ],
         ];
         $apiResponse->setParams($params);
         $response = $apiResponse->isBodyExistAndCorrectType($request);
         if ($apiResponse->hasError) {
             return $this->json($response, 400, ['Content-Type' => 'application/json']);
         }
-        $userAddress = new UserAddress();
+        $companyAddress = new CompanyAddress();
 
-        $user = $this->em->getRepository(User::class)->find($response["user_id"]);
-        $userAddress->setUserId($user);
+        $company = $this->em->getRepository(Company::class)->find($response["company_id"]);
+        $companyAddress->setCompanyId($company);
         
-        $name = $response["name"] ?? null;
-        if ($name) $userAddress->setName($name);
+        $name = $response["name"];
+        $companyAddress->setName($name);
 
-        $userAddress->setAddressLine1($response["address_line1"]);
+        $companyAddress->setAddressLine1($response["address_line1"]);
 
         $addressLine2 = $response["address_line2"] ?? null;
-        if ($addressLine2) $userAddress->setAddressLine2($addressLine2);
+        if ($addressLine2) $companyAddress->setAddressLine2($addressLine2);
 
-        $userAddress->setCity($response["city"]);
-        $userAddress->setPostalCode($response["postal_code"]);
-        $userAddress->setCountry($response["country"]);
-        $userAddress->setPhoneNumber($response["phone_number"]);
+        $companyAddress->setCity($response["city"]);
+        $companyAddress->setPostalCode($response["postal_code"]);
+        $companyAddress->setCountry($response["country"]);
 
-        $userAddress->setCreatedAt(new DateTimeImmutable());
-        $userAddress->setUpdatedAt(new DateTimeImmutable());
+        $companyAddress->setCreatedAt(new DateTimeImmutable());
+        $companyAddress->setUpdatedAt(new DateTimeImmutable());
 
-        $this->em->persist($userAddress);
+        $this->em->persist($companyAddress);
         $this->em->flush();
         return $this->json(true);
     }
     
-    #[Route('/users/addresses/{id}', methods: "POST")]
+    #[Route('/companies/addresses/{id}', methods: "POST")]
     public function updateAdresses(int $id, Request $request, ApiResponse $apiResponse): Response
     {
-        $userAddress = $this->em->getRepository(UserAddress::class)->find($id);
+        $companyAddress = $this->em->getRepository(CompanyAddress::class)->find($id);
 
-        if (empty($userAddress)) {
+        if (empty($companyAddress)) {
             return $this->json("Doesn't exist", 404);
         }
 
         $params = [
+            "company_id" => [
+                "type" => "integer",
+                "required" => false
+            ],
             "name" => [
                 "type" => "string",
                 "required" => false
@@ -135,10 +134,6 @@ class UserAddressController extends AbstractController
                 "type" => "string",
                 "required" => false
             ],
-            "phone_number" => [
-                "type" => "string",
-                "required" => false
-            ],
         ];
         $apiResponse->setParams($params);
         $response = $apiResponse->isBodyExistAndCorrectType($request);
@@ -147,49 +142,35 @@ class UserAddressController extends AbstractController
         }
         
 
+        $companyId = $response["company_id"] ?? null;
+        if ($companyId) {
+            $company = $this->em->getRepository(Company::class)->find($companyId);
+            $companyAddress->setCompanyId($company);
+        }
+
         $name = $response["name"] ?? null;
-        if ($name) $userAddress->setName($name);
+        if ($name) $companyAddress->setName($name);
 
         $addressLine1 = $response["address_line1"] ?? null;
-        if ($addressLine1) $userAddress->setAddressLine1($addressLine1);
+        if ($addressLine1) $companyAddress->setAddressLine1($addressLine1);
 
         $addressLine2 = $response["address_line2"] ?? null;
-        if ($addressLine2) $userAddress->setAddressLine2($addressLine2);
+        if ($addressLine2) $companyAddress->setAddressLine2($addressLine2);
 
         $city = $response["city"] ?? null;
-        if ($city) $userAddress->setCity($city);
+        if ($city) $companyAddress->setCity($city);
 
         $postalCode = $response["postal_code"] ?? null;
-        if ($postalCode) $userAddress->setPostalCode($postalCode);
+        if ($postalCode) $companyAddress->setPostalCode($postalCode);
 
         $country = $response["country"] ?? null;
-        if ($country) $userAddress->setCountry($country);
+        if ($country) $companyAddress->setCountry($country);
 
-        $phoneNumber = $response["phone_number"] ?? null;
-        if ($phoneNumber) $userAddress->setPhoneNumber($phoneNumber);
+        $companyAddress->setUpdatedAt(New DateTimeImmutable());
 
-        $userAddress->setSelectedAt(New DateTimeImmutable());
-        $userAddress->setUpdatedAt(New DateTimeImmutable());
-
-        $this->em->persist($userAddress);
+        $this->em->persist($companyAddress);
         $this->em->flush();
 
         return $this->json(true);
-    }
-    
-    #[Route('/users/addresses/last', methods: "GET", name: 'app_user_last_address')]
-    public function getLastAdresses(Request $request, ApiResponse $apiResponse): Response
-    {
-        $params = [
-            "user_id" => "integer"
-        ];
-        $apiResponse->setParams($params);
-        $response = $apiResponse->isParamsExistAndCorrectType($request);
-        if ($apiResponse->hasError) {
-            return $this->json($response, 400, ['Content-Type' => 'application/json']);
-        }
-        $userId = $response["user_id"];
-        $userAddress = $this->em->getRepository(UserAddress::class)->findLastByUser($userId);
-        return $this->json($userAddress);
     }
 }
